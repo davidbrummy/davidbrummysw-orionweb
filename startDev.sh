92ca9617e2d95b6907751abc0a33c99ff91c12c2
David J. Jones <davidbrummy@users.noreply.github.com>
2026-01-18 20:03:27 -0800

    move to type script

--- startDev.sh
+++ startDev.sh
@@ -2,4 +2,4 @@
 
 docker build . -t davidbrummysw-orion-web1.0
 
-docker run -it -p 3000:80 --network orion --name davidbrummysw-orion-web davidbrummysw-orion-web1.0
+docker run -it -p 80:80 --network orion --name davidbrummysw-orion-web davidbrummysw-orion-web1.0
